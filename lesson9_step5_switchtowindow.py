b6c94a46a7867cf3d9e5821fc81cbe9141365f09
ilona <ilona.pashykian@loopme.com>
2022-01-19 21:13:22 +0200

    switch to window task

---
+++ lesson9_step5_switchtowindow.py
@@ -0,0 +1,37 @@
+from selenium import webdriver
+import time
+import math
+
+
+def calc(x):
+  return str(math.log(abs(12*math.sin(int(x)))))
+
+
+try:
+    link = "http://suninjuly.github.io/redirect_accept.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    button = browser.find_element_by_css_selector("button.trollface")
+    button.click()
+
+    new_window = browser.window_handles[1]
+    browser.switch_to.window(new_window)
+
+    x_element = browser.find_element_by_id("input_value")
+    x = x_element.text
+    y = calc(x)
+
+    input_val = browser.find_element_by_id("answer")
+    input_val.send_keys(y)
+
+    button_1 = browser.find_element_by_css_selector("button.btn")
+    button_1.click()
+
+finally:
+    print(browser.switch_to.alert.text)
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
+
